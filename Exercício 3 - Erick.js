a36fe1fa2a0e8cfbffb3ad433968ac417585db5d
Hudson_Nunes <55281979+Hudson-Nunes@users.noreply.github.com>
2019-10-03 23:36:04 -0300

    Add files via upload

--- Exercício 3 - Erick.js
+++ Exercício 3 - Erick.js
@@ -1,51 +1,41 @@
 var novoSalario;
 var codigo;
-var total = 0;
-var diferenca = 0;
+var total=0;
+var diferenca=0;
 
-do {
-    var cargo = prompt(' Digite o cod do cargo' +
-        '\n Cargos disponíveis:' +
-        '\n 101 - Gerente' +
-        '\n 102 - Engenheiro' +
-        '\n 103 - Técnico' +
-        '\n Outros');
-    if (cargo == "") {
-        alert('Cargo indefinido! Favor insira o cargo.' +
-        '\n Cargos disponíveis:' +
-        '\n 101 - Gerente' +
-        '\n 102 - Engenheiro' +
-        '\n 103 - Técnico' +
-        '\n Outros');
-    } else {
-        var salario = Number(prompt(' Digite o salario '));
-    }
-} while (cargo == "");
+
+
+var salario=Number(prompt(' Digite o salario '));
+var cargo=prompt(' Digite o cod do cargo ');
 
 
 switch (cargo) {
     case "101":
-        total = salario + (salario * (10 / 100)); /*  ou   total= salario + 1.10);  */
-        diferenca = Number(total) - salario;
+        total= salario +(salario * (10/100));
+        diferenca = Number(total)-salario;
 
         break;
     case "102":
-        total = salario + (salario * (20 / 100))  /*  ou   total= salario + 1.20);  */
-        diferenca = Number(total) - salario;
+        salario += salario  * (20/100);
+        diferenca = Number(total)-salario;
         break;
     case "103":
-        total = salario + (salario * (30 / 100))  /*  ou   total= salario + 1.30);  */
-        diferenca = Number(total) - salario;
+        salario += salario * (30/100);
+        diferenca = Number(total)-salario;
         break;
     default:
-        total = salario + (salario * (40 / 100))  /*  ou   total= salario + 1.40);  */
-        diferenca = Number(total) - salario;
+        salario += salario * (40/100);
+        diferenca = Number(total)-salario;
 }
 
-alert("salario:" + salario);
-alert("Total:" + total);
-alert("Diferença:" + diferenca);
+alert(+salario, +total, +diferenca);
 
 
 
 
+/*function calculaSalario(){
+        var valor = prompt("informe o valor");
+        var quantidade = document.getElementById("quantidade");
+        var salario =  Number(valor*quantidade);
+        alert(salario);
+    }*/
